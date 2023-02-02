3fdec3b0de23af3154d03d8fc99891ccf9e0f695
Alexander Lobakin <alobakin@mailbox.org>
2023-02-02 23:17:27 +0100

    lrng: kill @urandom_fops
    Now that /dev/{,u}random use the same blocking fops, remove
    @urandom_fops from the LRNG code.
    
    Signed-off-by: Alexander Lobakin <alobakin@mailbox.org>

--- drivers/char/lrng/lrng_interface_random_user.c
+++ drivers/char/lrng/lrng_interface_random_user.c
@@ -14,32 +14,10 @@
 #include "lrng_es_mgr.h"
 #include "lrng_interface_dev_common.h"
 
-static ssize_t lrng_drng_read(struct file *file, char __user *buf,
-			      size_t nbytes, loff_t *ppos)
-{
-	if (!lrng_state_min_seeded())
-		pr_notice_ratelimited("%s - use of insufficiently seeded DRNG (%zu bytes read)\n",
-				      current->comm, nbytes);
-	else if (!lrng_state_operational())
-		pr_debug_ratelimited("%s - use of not fully seeded DRNG (%zu bytes read)\n",
-				     current->comm, nbytes);
-
-	return lrng_read_common(buf, nbytes, false);
-}
-
 const struct file_operations random_fops = {
 	.read  = lrng_drng_read_block,
 	.write = lrng_drng_write,
 	.poll  = lrng_random_poll,
-	.unlocked_ioctl = lrng_ioctl,
-	.compat_ioctl = compat_ptr_ioctl,
-	.fasync = lrng_fasync,
-	.llseek = noop_llseek,
-};
-
-const struct file_operations urandom_fops = {
-	.read  = lrng_drng_read,
-	.write = lrng_drng_write,
 	.unlocked_ioctl = lrng_ioctl,
 	.compat_ioctl = compat_ptr_ioctl,
 	.fasync = lrng_fasync,
@@ -89,7 +67,7 @@ SYSCALL_DEFINE3(getrandom, char __user *, buf, size_t, count,
 		count = INT_MAX;
 
 	if (flags & GRND_INSECURE) {
-		return lrng_drng_read(NULL, buf, count, NULL);
+		return lrng_read_common(buf, count, false);
 	} else if (flags & GRND_SEED) {
 		unsigned int seed_flags = (flags & GRND_NONBLOCK) ?
 					  LRNG_GET_SEED_NONBLOCK : 0;
